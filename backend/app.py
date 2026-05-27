77e7f72e57c5f1be3caf17255a60776cba23b93f
HARIPRIYA-JS~ <haripriyajegadeshwaran@gmail.com>
2026-05-27 01:10:14 -0400

    fixed result page

--- backend/app.py
+++ backend/app.py
@@ -1,16 +1,21 @@
 from flask import Flask, request, jsonify, render_template
 import joblib
 from pymongo import MongoClient
+import os
 
 app = Flask(__name__)
+
 @app.route("/")
 def home():
     return render_template("index.html")
+
 client = MongoClient("mongodb://localhost:27017/")
 db = client["scam_soc"]
 collection = db["alerts"]
 
 model = joblib.load('model/scam_detector.pkl')
+
+
 @app.route('/predict', methods=['POST'])
 def predict():
 
@@ -18,15 +23,13 @@ def predict():
 
     result = model.predict([msg])[0]
 
-    return f"""
-    <h2>URL: {msg}</h2>
-    <h1>Prediction: {result}</h1>
-    <a href="/">Check Another URL</a>
-    """
+    return render_template(
+        "result.html",
+        url=msg,
+        prediction=str(result).lower()
+    )
 
-    
-import os
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT",5000))
     app.run(host="0.0.0.0", port=port)
